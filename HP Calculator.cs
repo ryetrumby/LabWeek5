f79e984759ac6383d03d5893bb981997d7322fac
Rylan <149338065+ryetrumby@users.noreply.github.com>
2024-02-08 14:47:59 -0500

    Add files via upload

---
+++ HP Calculator.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPCalculator : MonoBehaviour
+{
+    [SerializeField] private int maxHP;
+    [SerializeField] private int charLevel;
+    [SerializeField] private int constScore;
+    [SerializeField] private int diceToUse;
+    [SerializeField] private float avgHitDie;
+    [SerializeField] private string charClass = "Artificer";
+    [SerializeField] private string charName;
+    [SerializeField] private bool hillDwarf;
+    [SerializeField] private bool tough;
+    [SerializeField] private bool averaged;
+
+    Dictionary<string, int> dieClass =
+        new Dictionary<string, int>()
+        {
+            {"Artificer", 8 },
+            {"Barbarian", 12 },
+            {"Bard", 8 },
+            {"Cleric", 8 },
+            {"Druid", 8 },
+            {"Fighter", 10 },
+            {"Monk", 8 },
+            {"Ranger", 10 },
+            {"Rogue", 8 },
+            {"Paladin", 10 },
+            {"Sorcerer", 6 },
+            {"Wizard", 6 },
+            {"Warlock", 8 }
+        };
+
+    Dictionary<int, int> constModifier =
+        new Dictionary<int, int>()
+        {
+            {1, -5 },
+            {2, -4 },
+            {3, -4 },
+            {4, -3 },
+            {5, -3 },
+            {6, -2 },
+            {7, -2 },
+            {8, -1 },
+            {9, -1 },
+            {10, 0 },
+            {11, 0 },
+            {12, 1 },
+            {13, 1 },
+            {14, 2 },
+            {15, 2 },
+            {16, 3 },
+            {17, 3 },
+            {18, 4 },
+            {19, 4 },
+            {20, 5 },
+            {21, 5 },
+            {22, 6 },
+            {23, 6 },
+            {24, 7 },
+            {25, 7 },
+            {26, 8 },
+            {27, 8 },
+            {28, 9 },
+            {29, 9 },
+            {30, 10 }
+        };
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log = diceToUse;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void checkForCharClass()
+        if (dieClass.ContainsKey(charClass))
+        {
+        diceToUse = dieClass[charClass];
+        {
+        else
+        {
+        Debug.Log = "Class does not exist in 5e";
+        }
+}
