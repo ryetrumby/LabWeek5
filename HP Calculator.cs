19b21818f994bf6e7ee63d45abcf17c32336a6e7
lsaenzmarmol <lsaenzmarmol@gmail.com>
2024-02-11 02:35:06 -0500

    Fixed Script for Unity Inspector

--- HP Calculator.cs
+++ HP Calculator.cs
@@ -35,45 +35,45 @@ public class HPCalculator : MonoBehaviour
 
     Dictionary<int, int> constModifier =
         new Dictionary<int, int>()
-        {
-            {1, -5 },
-            {2, -4 },
-            {3, -4 },
-            {4, -3 },
-            {5, -3 },
-            {6, -2 },
-            {7, -2 },
-            {8, -1 },
-            {9, -1 },
-            {10, 0 },
-            {11, 0 },
-            {12, 1 },
-            {13, 1 },
-            {14, 2 },
-            {15, 2 },
-            {16, 3 },
-            {17, 3 },
-            {18, 4 },
-            {19, 4 },
-            {20, 5 },
-            {21, 5 },
-            {22, 6 },
-            {23, 6 },
-            {24, 7 },
-            {25, 7 },
-            {26, 8 },
-            {27, 8 },
-            {28, 9 },
-            {29, 9 },
-            {30, 10 }
-        };
+            {
+                {1, -5 },
+                {2, -4 },
+                {3, -4 },
+                {4, -3 },
+                {5, -3 },
+                {6, -2 },
+                {7, -2 },
+                {8, -1 },
+                {9, -1 },
+                {10, 0 },
+                {11, 0 },
+                {12, 1 },
+                {13, 1 },
+                {14, 2 },
+                {15, 2 },
+                {16, 3 },
+                {17, 3 },
+                {18, 4 },
+                {19, 4 },
+                {20, 5 },
+                {21, 5 },
+                {22, 6 },
+                {23, 6 },
+                {24, 7 },
+                {25, 7 },
+                {26, 8 },
+                {27, 8 },
+                {28, 9 },
+                {29, 9 },
+                {30, 10 }
+            };
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log = diceToUse;
+        Debug.Log("Uses this Dice: " + diceToUse);
     }
 
     // Update is called once per frame
@@ -82,12 +82,14 @@ public class HPCalculator : MonoBehaviour
 
     }
     void checkForCharClass()
+    {
         if (dieClass.ContainsKey(charClass))
-        {
-        diceToUse = dieClass[charClass];
-        {
-        else
-        {
-        Debug.Log = "Class does not exist in 5e";
-        }
+            {
+                diceToUse = dieClass[charClass];
+            }
+            else
+            {
+                Debug.Log("Class does not exist in 5e");
+            }
+    }
 }
